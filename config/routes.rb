b9358299eab489a7f1ef644bb375dd4ae4ed0765
Robby Dore <rdore88@gmail.com>
2017-07-27 11:28:51 -0400

    Created ability to log in and association between user and book

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :books
+  resources :users, only: [:new, :create, :show, :index]
+  get "/sessions" => "sessions#destroy"
+  resources :sessions, only: [:new, :create]
 
   root "books#index"
 end
